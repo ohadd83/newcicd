a083fbc9fc0b5d0b1e1963f91705ae0b67ef1c49
ohadd83 <ohad306@gmail.com>
2026-05-14 10:33:52 +0000

    change text in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end("Hello Jenkins CI/CD lets move on this pipeline push image to dockerhub today 4_5_2026");
+  res.end("Hello Jenkins CI/CD lets move on this pipeline push image to dockerhub today 14_5_2026");
 }).listen(3000);
 
